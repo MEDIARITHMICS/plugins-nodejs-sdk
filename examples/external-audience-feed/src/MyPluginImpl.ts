b3b8d62bc7a715191ffb4729ec9d9eb2974c5c06
ThomasDelalbre <tdelalbre.ext@mediarithmics.com>
2018-10-17 12:36:28 +0200

    adding general functions

--- examples/external-audience-feed/src/MyPluginImpl.ts
+++ examples/external-audience-feed/src/MyPluginImpl.ts
@@ -1,8 +1,13 @@
 import { core } from "@mediarithmics/plugins-nodejs-sdk";
 import { IExampleAudienceFeedConnector } from "./interfaces/IExampleAudienceFeedConnector";
 import { addEmailsInCustomAudience, createCustomAudience } from './services/ExampleAudienceFeed';
+import { ITechnicalConfiguration } from './interfaces/IMics';
+
 
 const API_KEY_PROPERTY_NAME: string = 'api-key';
+const TECHNICAL_CONFIGURATION_FILE_NAME = 'technical-configuration';
+const EXAMPLEAUDIENCEFEED_AUDIENCE_ID_PROPERTY_NAME = 'exampleaudiencefeed_audience_id';
+const EXAMPLEAUDIENCEFEED_AUDIENCE_NAME_PROPERTY_NAME = 'exampleaudiencefeed_audience_name';
 
 export default class ExampleAudienceFeedConnector extends core.AudienceFeedConnectorBasePlugin {
 
@@ -11,12 +16,15 @@ export default class ExampleAudienceFeedConnector extends core.AudienceFeedConne
     try {
         const baseInstanceContext: core.AudienceFeedConnectorBaseInstanceContext = await super.instanceContextBuilder(feedId);
         const apiKey: string = this.getApiKey(baseInstanceContext, feedId)
+        const exampleAudienceFeedAudienceId: string = this.getExampleAudienceFeedAudienceId(baseInstanceContext, feedId);
+        const exampleAudienceFeedAudienceName: string = this.getExampleAudienceFeedAudienceName(baseInstanceContext, feedId);
 
         const context = {
             api_key: apiKey,
+            audience_feed_id: exampleAudienceFeedAudienceId,
+            audience_feed_name: exampleAudienceFeedAudienceName,
             ...baseInstanceContext
         };
-        
 
         return context;
 
@@ -28,7 +36,7 @@ export default class ExampleAudienceFeedConnector extends core.AudienceFeedConne
 
     protected async onExternalSegmentCreation(
         request: core.ExternalSegmentCreationRequest,
-        instanceContext: ExampleAudienceFeedConnectorConnectorInstanceContext
+        instanceContext: IExampleAudienceFeedConnector
     ): Promise<core.ExternalSegmentCreationPluginResponse> {
         try {
             this.logger.debug(`Feed ${request.feed_id} - Segment ${request.segment_id} - OnExternalSegmentCreation`);
@@ -38,8 +46,8 @@ export default class ExampleAudienceFeedConnector extends core.AudienceFeedConne
 
 
               await Promise.all(instanceContext.apps.map(async (app: ICredentialsClientApp) => {
-                this.logger.debug(`Email ${app.name} - Create custom audience`);
-                const payload: ExampleAudienceFeedPayloadCustomAudience = buildCreateCustomAudiencePayload(instanceContext.batchAudienceName as string);
+                this.logger.debug(`Create custom audience`);
+                const payload: ExampleAudienceFeedPayloadCustomAudience = buildCreateCustomAudiencePayload(instanceContext.ExampleAudienceFeedAudienceName as string);
                 await createCustomAudience(instanceContext, payload, this.logger);
               }));
 
@@ -87,16 +95,73 @@ export default class ExampleAudienceFeedConnector extends core.AudienceFeedConne
         }
     }
 
+
+
+
+
+
+
+    
+  private async getTechnicalConfiguration(feedId: string): Promise<ITechnicalConfiguration> {
+    const technicalConfigurationFile = await this.fetchConfigurationFile(TECHNICAL_CONFIGURATION_FILE_NAME);
+
+    if (!technicalConfigurationFile) {
+      throw new Error(`Feed: ${feedId} - Error in file fetch ${TECHNICAL_CONFIGURATION_FILE_NAME}`);
+    }
+
+    this.logger.debug('Log technical file :', JSON.stringify(technicalConfigurationFile.toString()));
+    const technicalConfiguration: ITechnicalConfiguration = JSON.parse(technicalConfigurationFile.toString());
+
+    if (!technicalConfiguration.mics_api_version) {
+      throw new Error(`Feed: ${feedId} - Missing configuration object: mics_api_version`);
+    }
+    if (!technicalConfiguration.mics_api_endpoint) {
+      throw new Error(`Feed: ${feedId} - Missing configuration object: mics_api_endpoint`);
+    }
+    return technicalConfiguration;
+  }
+
+
+
+
+    
+  private getExampleAudienceFeedAudienceId(
+    baseInstanceContext: core.AudienceFeedConnectorBaseInstanceContext,
+    feedId: string
+  ): string {
+    const exampleAudienceFeedAudienceId = baseInstanceContext.feedProperties.find(property => {
+      return property.technical_name == EXAMPLEAUDIENCEFEED_AUDIENCE_ID_PROPERTY_NAME;
+    }) as core.StringProperty;
+
+    if (!exampleAudienceFeedAudienceId || !exampleAudienceFeedAudienceId.value || exampleAudienceFeedAudienceId.value.value === undefined) {
+      throw new Error(`Feed: ${feedId} - Missing ${EXAMPLEAUDIENCEFEED_AUDIENCE_ID_PROPERTY_NAME} property`);
+    }
+
+    return exampleAudienceFeedAudienceId.value.value as string;
+  }
+
+  private getExampleAudienceFeedAudienceName(
+    baseInstanceContext: core.AudienceFeedConnectorBaseInstanceContext,
+    feedId: string
+  ): string {
+    const exampleAudienceFeedAudienceName = baseInstanceContext.feedProperties.find(property => {
+      return property.technical_name == EXAMPLEAUDIENCEFEED_AUDIENCE_NAME_PROPERTY_NAME;
+    }) as core.StringProperty;
+    if (!exampleAudienceFeedAudienceName || !exampleAudienceFeedAudienceName.value || exampleAudienceFeedAudienceName.value.value === undefined) {
+      throw new Error(`Feed: ${feedId} - Missing ${EXAMPLEAUDIENCEFEED_AUDIENCE_NAME_PROPERTY_NAME} property`);
+    }
+    return exampleAudienceFeedAudienceName.value.value as string;
+  }
+
+
     private getApiKey(
         baseInstanceContext: core.AudienceFeedConnectorBaseInstanceContext,
         feedId: string
     ): string {
         const apiKey = baseInstanceContext.feedProperties.findStringProperty(API_KEY_PROPERTY_NAME);
-
         if (!apiKey || !apiKey.value || apiKey.value.value === undefined) {
             throw new Error(`Feed: ${feedId} - Missing ${API_KEY_PROPERTY_NAME} property`);
         }
-
         return apiKey.value.value as string;
     }
 
