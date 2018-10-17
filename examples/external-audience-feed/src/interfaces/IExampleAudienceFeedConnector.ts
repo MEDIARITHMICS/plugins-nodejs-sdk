af5740972f53878a3020d5618eab462cfddc144a
ThomasDelalbre <tdelalbre.ext@mediarithmics.com>
2018-10-17 12:36:51 +0200

    little rewriting

--- examples/external-audience-feed/src/interfaces/IExampleAudienceFeedConnector.ts
+++ examples/external-audience-feed/src/interfaces/IExampleAudienceFeedConnector.ts
@@ -2,5 +2,5 @@ import { core } from "@mediarithmics/plugins-nodejs-sdk";
 
 
 export interface IExampleAudienceFeedConnector extends core.AudienceFeedConnectorBaseInstanceContext {
-    api_key: string;
+    exampleApiKey: string;
 }
