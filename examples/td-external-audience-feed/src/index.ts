b33758e81be272226739d345a04946ccdce72cfe
ThomasDelalbre <tdelalbre.ext@mediarithmics.com>
2018-12-17 11:33:56 +0100

    deleting folders

--- examples/td-external-audience-feed/src/index.ts
+++
@@ -1,8 +0,0 @@
-import ExampleAudienceFeedConnector from "./MyPluginImpl";
-import { core } from "@mediarithmics/plugins-nodejs-sdk";
-
-
-const plugin = new ExampleAudienceFeedConnector();
-const runner = new core.ProductionPluginRunner(plugin);
-
-runner.start();
