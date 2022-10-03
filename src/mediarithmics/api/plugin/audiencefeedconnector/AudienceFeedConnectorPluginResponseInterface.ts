be23425d2031bb36240eb27b478f21b3659d0951
F-Jules <faivre.jules@gmail.com>
2022-10-03 17:06:04 +0200

    replace tabs by spaces

--- src/mediarithmics/api/plugin/audiencefeedconnector/AudienceFeedConnectorPluginResponseInterface.ts
+++ src/mediarithmics/api/plugin/audiencefeedconnector/AudienceFeedConnectorPluginResponseInterface.ts
@@ -3,48 +3,48 @@ export type AudienceFeedConnectorContentType = 'text/csv' | 'application/json' |
 export declare type AudienceFeedConnectorConnectionStatus = 'ok' | 'error' | 'external_segment_not_ready_yet';
 
 export interface AudienceFeedConnectorPluginResponse {
-	status: AudienceFeedConnectorStatus;
-	data?: UserSegmentUpdatePluginResponseData[];
-	stats?: UserSegmentUpdatePluginResponseStats[];
-	message?: string;
+  status: AudienceFeedConnectorStatus;
+  data?: UserSegmentUpdatePluginResponseData[];
+  stats?: UserSegmentUpdatePluginResponseStats[];
+  message?: string;
 }
 
 export interface ExternalSegmentCreationPluginResponse {
-	status: AudienceFeedConnectorStatus;
-	message?: string;
+  status: AudienceFeedConnectorStatus;
+  message?: string;
 }
 
 export interface ExternalSegmentConnectionPluginResponse {
-	status: AudienceFeedConnectorConnectionStatus;
-	message?: string;
+  status: AudienceFeedConnectorConnectionStatus;
+  message?: string;
 }
 
 export interface UserSegmentUpdatePluginResponse {
-	status: AudienceFeedConnectorStatus;
-	data?: UserSegmentUpdatePluginResponseData[];
-	stats?: UserSegmentUpdatePluginResponseStats[];
-	message?: string;
-	nextMsgDelayInMs?: number;
+  status: AudienceFeedConnectorStatus;
+  data?: UserSegmentUpdatePluginResponseData[];
+  stats?: UserSegmentUpdatePluginResponseStats[];
+  message?: string;
+  nextMsgDelayInMs?: number;
 }
 
 export interface UserSegmentUpdatePluginResponseData {
-	grouping_key?: string;
-	content?: string;
-	binary_content?: BinaryType;
+  grouping_key?: string;
+  content?: string;
+  binary_content?: BinaryType;
 }
 
 export interface UserSegmentUpdatePluginFileDeliveryResponseData extends UserSegmentUpdatePluginResponseData {
-	type: 'FILE_DELIVERY';
-	destination_token?: string;
+  type: 'FILE_DELIVERY';
+  destination_token?: string;
 }
 
 export interface UserSegmentUpdatePluginBatchDeliveryResponseData extends UserSegmentUpdatePluginResponseData {
-	type: 'BATCH_DELIVERY';
-	batch_token?: string;
+  type: 'BATCH_DELIVERY';
+  batch_token?: string;
 }
 
 export interface UserSegmentUpdatePluginResponseStats {
-	identifier?: string;
-	sync_result?: string;
-	tags?: any;
+  identifier?: string;
+  sync_result?: string;
+  tags?: any;
 }
