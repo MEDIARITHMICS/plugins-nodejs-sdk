99fac9c028e56b65427273f5ba577edb129ebd73
Nicolas Guillomon <43535051+nguillomon@users.noreply.github.com>
2024-06-27 14:21:49 +0200

    Revert "MICS-18376 sdk-computedField"

--- examples/computed-field/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { core } from '@mediarithmics/plugins-nodejs-sdk';
-import { MyComputedField } from './MyComputedField';
-
-// All the magic is here
-const plugin = new MyComputedField();
-const runner = new core.ProductionPluginRunner(plugin);
-
-runner.start();
